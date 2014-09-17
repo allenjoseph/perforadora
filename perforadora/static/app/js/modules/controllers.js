bccd86a33e4d9f7cfb9066c1b369d34bf0a4001f
AllenJoseph <allen.joseph.v.a@gmail.com>
2014-09-16 23:48:32 -0500

    update app

--- perforadora/static/app/js/modules/controllers.js
+++ perforadora/static/app/js/modules/controllers.js
@@ -4,39 +4,47 @@ define(['angular'],function(){
 
     var appControllers = angular.module('appControllers',[]);
 
-    appControllers.controller('MainCtrl',['$scope', 'MainService',
-    function($scope, MainService){
-        $scope.modulos = MainService.query();
-    }]);
-
-    appControllers.controller('PlanillaListCtrl',['$scope', 'PlanillaService',
-    function($scope, PlanillaService){
-        $scope.planilla = PlanillaService.query();
-    }]);
-
-    appControllers.controller('MenuCtrl', ['$scope', 'MenuService',
-    function($scope, MenuService){
+    appControllers.controller('MenuCtrl', ['$scope', 'MenuService', function($scope, MenuService){
         var ng = $scope;
 
         ng.modulos = MenuService.query();
-
         ng.menuActive = 'home';
-
         ng.selectMenu = function(menu){
-
             ng.menuActive = menu;
         }
     }]);
 
-    appControllers.controller('TrabajadorListCtrl', ['$scope', 'TrabajadorService',
-    function($scope, TrabajadorService){
-        $scope.modulo =
+    appControllers.controller('MainCtrl',['$scope', 'MainService', function($scope, MainService){
+        var ng = $scope;
+
+        ng.modulo =
+        {
+            titulo : 'Home',
+            modulos : MainService.query()
+        };
+
+    }]);
+
+    appControllers.controller('PlanillaListCtrl',['$scope', 'PlanillaService', function($scope, PlanillaService){
+        var ng = $scope;
+
+        ng.modulo =
+        {
+            titulo : 'Administrar Planilla',
+            planillas : PlanillaService.query()
+        };
+    }]);
+
+    appControllers.controller('TrabajadorListCtrl', ['$scope', 'TrabajadorService', function($scope, TrabajadorService){
+        var ng = $scope;
+
+        ng.modulo =
         {
             titulo : 'Administrar Trabajador',
             trabajadores : TrabajadorService.query()
         };
 
-        $scope.modulo.datatable = {
+        ng.modulo.datatable = {
             header : [  { id : 'id_trabajador', label : 'ID'},
                         { id : 'apellido_paterno', label : 'APELLIDO'},
                         { id : 'nombres', label : 'NOMBRE'},
